9070c8205d128bc58d83f63979dbeebf805bb05b
IraJyliaLena <irishka16_02@mail.ru>
2015-11-09 11:34:57 +0500

    Проект изменен

--- PetShop/PetShop/frmProviders.cs
+++ PetShop/PetShop/frmProviders.cs
@@ -96,23 +96,60 @@ namespace PetShop
             fillTheTable(dgvp, "DISPLAY_PROVIDER");
         }
 
-        private void butDel_Click(object sender, EventArgs e)
+        int GetKolBreeds(int id)
         {
-            string connectionString = @"Data Source=.;Initial Catalog=PetShopO;user id=sa; password=1;";
-            myConnection = new SqlConnection(connectionString);
+            int kol = 0;
+            string query = @"select count(provider_id) from Breeds where provider_id = '1'";
             try
             {
-                myConnection.Open();
+                string connectionString = @"Data Source=.;Initial Catalog=PetShopO;user id=sa; password=1;";
+                myConnection = new SqlConnection(connectionString);
+                try
+                {
+                    myConnection.Open();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message); ;
+                }
+                using (var cmd = myConnection.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = string.Format(query, id);
+                    object value = cmd.ExecuteScalar();
+                    cmd.ExecuteNonQuery();
+                    kol = Convert.ToInt32(value.ToString());
+                }
+                return kol;
             }
-            catch (SqlException ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
+                return kol;
             }
-            var sqlCmd = new SqlCommand("delete_from_provider", myConnection);
-            sqlCmd.CommandType = CommandType.StoredProcedure;
-            sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(dgvProviders.SelectedCells[0].Value));
-            sqlCmd.ExecuteNonQuery();
-            fillTheTable(dgvProviders, "DISPLAY_PROVIDER");
+        }
+
+        private void butDel_Click(object sender, EventArgs e)
+        {
+            int kol = GetKolBreeds(Convert.ToInt32(dgvProviders.SelectedCells[0].Value));
+            if (kol == 0)
+            {
+                string connectionString = @"Data Source=.;Initial Catalog=PetShopO;user id=sa; password=1;";
+                myConnection = new SqlConnection(connectionString);
+                try
+                {
+                    myConnection.Open();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                var sqlCmd = new SqlCommand("delete_from_provider", myConnection);
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(dgvProviders.SelectedCells[0].Value));
+                sqlCmd.ExecuteNonQuery();
+                fillTheTable(dgvProviders, "DISPLAY_PROVIDER");
+            } else MessageBox.Show("Данного поставщика удалить невозможно.");            
         }
     }
 }
